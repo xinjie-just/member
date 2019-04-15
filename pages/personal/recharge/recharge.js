1b278bb4b821ed6b881da52c939e76ce5f6627f6
xinjie-just <18227752005@qq.com>
2019-04-15 22:11:03 +0800

    新增选择会员卡和在线充值页面，修改页面的跳转链接

---
+++ pages/personal/recharge/recharge.js
@@ -0,0 +1,88 @@
+// pages/personal/recharge/recharge.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    radioItems: [
+      { name: '会员卡1 余额200', value: '0' },
+      { name: '会员卡2 余额400', value: '1', checked: true }
+    ]
+  },
+
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radioItems;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      radioItems: radioItems
+    });
+  },
+
+  next() {
+    wx.navigateTo({
+      url: "sum/sum"
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
